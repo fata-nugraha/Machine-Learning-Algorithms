6887a1b8b69994598fd54ffbef00285f41189411
Pyroblazer <Pyroblazer@users.noreply.github.com>
2020-02-14 00:11:05 +0700

    I FEEL MORE BLESSED

--- code/main.py
+++ code/main.py
@@ -16,67 +16,61 @@ pd.options.mode.chained_assignment = None
 df = pd.read_csv('../datasets/iris.csv', sep=',')
 attributes = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
 target = 'species'
+# df = df.drop(target,axis=1)
 # -------------------------------------------------------------
 # df = pd.read_csv('../datasets/play_tennis.csv')
 # df = df.drop('day', axis=1)
 # attributes = ["outlook", "temp", "humidity", "wind"]
 # target = 'play'
-x = df.drop(target,axis=1)
-y = df[target]
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3)
-train = x_train
-train[target] = y_train
-test = x_test
-test[target] = y_test
-train = train.reset_index(drop=True)
+
+#separate the trained data and the test data
+# x = df.drop(target,axis=1)
+# y = df[target]
+# x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
+# train = x_train
+# train[target] = y_train
+# test = x_test
+# test[target] = y_test
+# train = train.reset_index(drop=True)
 
 
-def acc(tree, test):
-	rules = tree.getRules()
-	testcopy = test.reset_index(drop=True)
-	point = 0
-	for r in range(len(rules)):
-		for i in range(len(testcopy)):
-			check = True
-			output = False
-			for attribute in attributes:
-				try:
-					check = testcopy.iloc[i][attribute] == rules[r][attribute] and check
-				except Exception as e:
-					pass
-			if (check):
-				output = testcopy.iloc[i][target] == rules[r]['class']
-				point -=1
-			if (output):
-				point += 5
-		# if (checker>0):
-		# 	rules[r]['accuracy'] = accuracy/checker
-		# else:
-		# 	rules[r]['accuracy'] = 0.01
-	# rules = sorted(rules, key = lambda i: i['accuracy'], reverse = True)
-	# for rule in rules:
-		# print(rule)
-	return point
+# def accuracy(tree, test):
+# 	rules = tree.getRules()
+# 	testcopy = test.reset_index(drop=True)
+# 	point = 0
+# 	for r in range(len(rules)):
+# 		for i in range(len(testcopy)):
+# 			check = True
+# 			output = False
+# 			for attribute in attributes:
+# 				try:
+# 					check = testcopy.iloc[i][attribute] == rules[r][attribute] and check
+# 				except Exception as e:
+# 					pass
+# 			if (check):
+# 				output = testcopy.iloc[i][target] == rules[r]['class']
+# 				point -=1
+# 			if (output):
+# 				point += 5
+# 		# if (checker>0):
+# 		# 	rules[r]['accuracy'] = accuracy/checker
+# 		# else:
+# 		# 	rules[r]['accuracy'] = 0.01
+# 	# rules = sorted(rules, key = lambda i: i['accuracy'], reverse = True)
+# 	# for rule in rules:
+# 		# print(rule)
+# 	return point
 
-def multiprune(tree, test):
-	result = []
-	copytree = None
-	for i in range(len(tree.children)):
-		copytree = copy.deepcopy(tree)
-		if (copytree.children[i].isLeafsParent()):
-			copytree.children[i].prune()
-		if acc(copytree, test) > acc(tree, test):
-			result.append(copy.deepcopy(copytree))
-	return result
+# def multiprune(tree, test):
+# 	result = []
+# 	copytree = None
+# 	for i in range(len(tree.children)):
+# 		copytree = copy.deepcopy(tree)
+# 		if (copytree.children[i].isLeafsParent()):
+# 			copytree.children[i].prune()
+# 		if accuracy(copytree, test) > accuracy(tree, test):
+# 			result.append(copy.deepcopy(copytree))
+# 	return result
 
-
-
-c45 = myC45(train, target, attributes)
-tree = c45.id3.tree_
-print("INI TREE AWAL")
-tree.export_tree()
-print(acc(tree, test))
-print("===========")
-result = multiprune(tree, test)
-if result != []:
-	result[0].export_tree()
+c45 = myC45(df, target, attributes)
+# print("===========")
--- code/myc45.py
+++ code/myc45.py
@@ -1,7 +1,8 @@
 from collections import Counter
 from myid3 import *
+from sklearn.model_selection import train_test_split
 
-from copy import copy
+import copy
 
 import numpy as np
 pd.options.mode.chained_assignment = None
@@ -19,23 +20,98 @@ class myC45(myID3):
         for attribute in attributes:
             if (handledExamples[attribute].dtype == np.float64 or handledExamples[attribute].dtype == np.int64):
                 continuous_attributes.append(attribute)
-        print(continuous_attributes)
         
         bestTempExamples = self.splitAttributes(handledExamples, target_attribute, continuous_attributes)
-        self.id3 = myID3(bestTempExamples, target_attribute, attributes)
-                
-    def gainRatio(self, examples, target_attribute, attribute, classEntropy):
-        gain = self.getInformationGain(examples, target_attribute, returnAttr, classEntropy)
-        splitInformation = self.getSplitInformation(examples, target_attribute, attribute)
-        return gain/splitInformation
 
-    def getSplitInformation(self, examples, target_attribute, attribute):
-        classFreqRatios = examples[attribute].value_counts(normalize=True)
-        splitInformation = 0
-        for index in range (0, len(classFreqRatios)):
-            value = classFreqRatios.keys()[index]
-            splitInformation -= classFreqRatios[value] * self.getAttributeEntropy(examples, target_attribute, attribute, value)
-        return splitInformation
+        x = bestTempExamples.drop(target_attribute,axis=1)
+        y = bestTempExamples[target_attribute]
+        x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
+        train = x_train
+        train[target_attribute] = y_train
+        test = x_test
+        test[target_attribute] = y_test
+        train = train.reset_index(drop=True)
+
+        self.id3 = myID3(train, target_attribute, continuous_attributes)
+        
+        #initialize tree
+        tree = self.id3.tree_
+
+        #prune tree
+        result = self.multiprune(tree, test, target_attribute, continuous_attributes)
+        if result != []:
+            #print pruned tree
+            result[0].export_tree()
+        else:
+            tree.export_tree()
+            #print tree awal
+            # print(self.accuracy(tree, test, target_attribute, continuous_attributes))
+
+    ## pruning
+    #separate the trained data and the test data
+    def separateTrainedData(self, examples, target):
+        x = examples.drop(target,axis=1)
+        y = examples[target]
+        x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
+        train = x_train
+        train[target] = y_train
+        test = x_test
+        test[target] = y_test
+        train = train.reset_index(drop=True)
+    
+    # accuracy of data
+    def accuracy(self, tree, test, target_attribute, attributes):
+        rules = tree.getRules()
+        testcopy = test.reset_index(drop=True)
+        point = 0
+        for r in range(len(rules)):
+            for i in range(len(testcopy)):
+                check = True
+                output = False
+                for attribute in attributes:
+                    try:
+                        check = testcopy.iloc[i][attribute] == rules[r][attribute] and check
+                    except Exception as e:
+                        pass
+                if (check):
+                    output = testcopy.iloc[i][target_attribute] == rules[r]['class']
+                    point -=1
+                if (output):
+                    point += 5
+            # if (checker>0):
+            # 	rules[r]['accuracy'] = accuracy/checker
+            # else:
+            # 	rules[r]['accuracy'] = 0.01
+        # rules = sorted(rules, key = lambda i: i['accuracy'], reverse = True)
+        # for rule in rules:
+            # print(rule)
+        return point
+    
+    def multiprune(self, tree, test, target_attribute, attributes):
+        result = []
+        copytree = None
+        for i in range(len(tree.children)):
+            copytree = copy.deepcopy(tree)
+            if (copytree.children[i].isLeafsParent()):
+                copytree.children[i].prune()
+            if self.accuracy(copytree, test, target_attribute, attributes) > self.accuracy(tree, test, target_attribute, attributes):
+                result.append(copy.deepcopy(copytree))
+        return result
+
+    ##
+                
+    # def gainRatio(self, examples, target_attribute, attribute, classEntropy):
+    #     gain = self.getInformationGain(examples, target_attribute, returnAttr, classEntropy)
+    #     splitInformation = self.getSplitInformation(examples, target_attribute, attribute)
+    #     return gain/splitInformation
+
+    # def getSplitInformation(self, examples, target_attribute, attribute):
+    #     classFreqRatios = examples[attribute].value_counts(normalize=True)
+    #     splitInformation = 0
+    #     for index in range (0, len(classFreqRatios)):
+    #         value = classFreqRatios.keys()[index]
+    #         splitInformation -= classFreqRatios[value] * self.getAttributeEntropy(examples, target_attribute, attribute, value)
+    #     return splitInformation
     
     def handleMissingValues(self, df):
         for data in df:
@@ -43,7 +119,7 @@ class myC45(myID3):
         return df
 
     def changeContinuousAttributeValues(self, examples, attribute, threshold):
-        tempExamples = copy(examples)
+        tempExamples = copy.copy(examples)
         for index in range(len(examples[attribute])):
             tempExamples[attribute][index] = ' <= ' + str(threshold) if tempExamples[attribute][index] <= threshold else ' > ' + str(threshold)
         return tempExamples
@@ -60,7 +136,7 @@ class myC45(myID3):
         for attribute in attributes:
             maxGain = -1*float("inf")
             #Sort data according to the column
-            print(attribute)
+            # print(attribute)
             sortedExamples = bestTempExamples.sort_values(attribute)
             #For all the example
             for index in range(0, len(sortedExamples)-1):
@@ -71,11 +147,8 @@ class myC45(myID3):
                     gain = self.getInformationGain(tempExamples, target_attribute, attribute, classEntropy)
                     if gain >= maxGain:
                         maxGain = gain
-                        bestTempExamples = copy(tempExamples)
+                        bestTempExamples = copy.copy(tempExamples)
         return bestTempExamples
-
-    # def postPruning(overfitDecisionTree):
-    #     pass
 
     # 1. Infer the decision tree from the training set, growing the tree until the training 
     #    data is fit as well as possible and allowing overfitting to occur. 
